ec1c46371a4b293f17cb0179ca7ed0a1c3de5b28
Romain Ricord <romain.ricord.pro@gmail.com>
2022-06-27 13:46:38 +0200

    Delete

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
 import React, { useState, useEffect, createContext } from 'react';
 import { View, Text,Button,TextInput,StyleSheet } from 'react-native';
 
-import Navigation from './src/Navigation/Navigation';
-import UserComponent from './src/Components/UserComponent';
-import UserListComponent from './src/Components/UserListComponent';
-
-import TransactionComponent from './src/Components/TransactionComponent';
-import HomeScreen from './src/Screens/HomeScreen';
-import ChartComponent from './src/Components/ChartComponent';
-
-import IncomesFScreens from './src/Screens/IncomesFScreens';
-
-import ContScreen from './src/Screens/ContScreen';
-import IncomesFScreen from './src/Screens/IncomesFScreens';
-
-import StatScreen from './src/Screens/StatScreen';
-
 import AppBarBottom from './src/Navigation/AppBarBottom';
 import auth from '@react-native-firebase/auth';
 
