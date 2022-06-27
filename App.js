9105a285b15fd2953e8f175d45120222b74d6584
Romain Ricord <romain.ricord.pro@gmail.com>
2022-06-27 12:01:04 +0200

    Test google sign

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,Button } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 const App = () => {
   // Set an initializing state whilst Firebase connects
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
+
+  const onGoogleButtonPress = async () => {
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+  
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
 
   // Handle user state changes
   function onAuthStateChanged(user) {
@@ -22,8 +34,12 @@ const App = () => {
 
   if (!user) {
     return (
-      <View>
-        <Text>Login</Text>
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text>Login with Google</Text>
+        <Button
+          title="Google Sign-In"
+          onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+        />
       </View>
     );
   }
